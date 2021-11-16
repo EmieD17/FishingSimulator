345ae0e27bb02bf39a86f20feb3f0a9fa338d6d9
EmieD17 <Doucet.emie@gmail.com>
2021-11-16 11:21:34 -0500

    fishSpawnWorks & controleTactile en classe

--- World.cs
+++ World.cs
@@ -42,26 +42,7 @@ public class World : Node
 
         if(fishArray.Count < 6)
         {
-            // Choose a random location on Path2D.
-            var fishSpawnLocation = GetNode<PathFollow2D>("FishPath/FishSpawnLocation");
-            fishSpawnLocation.Offset = _random.Next();
-
-            // Create a Mob instance and add it to the scene.
-            KinematicBody2D fishInstance = (KinematicBody2D)fishScene.Instance();
-            GetNode<Node2D>("YSort/Fish").AddChild(fishInstance);
-
-            // Set the mob's direction perpendicular to the path direction.
-            float direction = fishSpawnLocation.Rotation + Mathf.Pi / 2;
-
-            // Set the mob's position to a random location.
-            fishInstance.Position = fishSpawnLocation.Position;
-
-            // Add some randomness to the direction.
-            direction += RandRange(-Mathf.Pi / 4, Mathf.Pi / 4);
-            fishInstance.Rotation = direction;
-
-            // Choose the velocity.
-            fishInstance.MoveAndCollide(new Vector2(RandRange(150f, 250f), 0).Rotated(direction));
+            createFish();
         }
         else{
             
@@ -69,10 +50,34 @@ public class World : Node
         }
 
     }
+    public void createFish(){
+        // Choose a random location on Path2D.
+            var fishSpawnLocation = GetNode<PathFollow2D>("FishPath/FishSpawnLocation");
+            fishSpawnLocation.Offset = _random.Next();
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+            // Create a Mob instance and add it to the scene.
+            FishShadow fishInstance = (FishShadow)fishScene.Instance();
+            GetNode<Node2D>("YSort/Fish").AddChild(fishInstance);
+
+            // Set the mob's position to a random location.
+            fishInstance.Position = fishSpawnLocation.Position;
+
+            // Add some randomness to the direction.
+            /*Vector2 direction = new Vector2(_random.Next(), _random.Next());
+            if(direction == new Vector2())
+                direction = new Vector2(0, 1);
+            fishInstance.set_direction(direction);
+            */
+
+            // Choose the velocity.
+            //fishInstance.MoveAndCollide(new Vector2(RandRange(150f, 250f), 0).Rotated(direction));
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        if(Input.IsActionJustPressed("ui_cancel")) {
+            GetTree().Quit();
+        }
+    }
 }
--- actors/FishShadow.cs
+++ actors/FishShadow.cs
@@ -3,17 +3,18 @@ using System;
 
 public class FishShadow : KinematicBody2D
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
-
     Vector2 velocity = Vector2.Zero;
+
     [Export]
     public int MinSpeed = 150; // Minimum speed range.
 
     [Export]
     public int MaxSpeed = 250; // Maximum speed range.
     static private Random _random = new Random();
+    [Export]
+    public Boolean isDebug = true;
+
+    Label debugLabel;
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -27,12 +28,23 @@ public class FishShadow : KinematicBody2D
         float scaleNb = (float)(_random.NextDouble() * (2.5 - 1) + 1);
         Vector2 scale = new Vector2(scaleNb, scaleNb);
         this.Scale = scale;
+        isDebug = true;
         
+
+    }
+    public void debug(){
+        if(!isDebug){
+            return ;
+        }
+        if(debugLabel == null){
+            debugLabel = GetNode<Label>("debugLabel");
+        }
+        debugLabel.Text = Position.ToString();
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        debug();
+    }
 }
