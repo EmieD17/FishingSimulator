73d00b1393e54112316578446ac10bc8c56194d4
EmieD17 <Doucet.emie@gmail.com>
2021-11-18 12:27:57 -0500

    fish moves and pop

--- World.cs
+++ World.cs
@@ -6,72 +6,43 @@ public class World : Node
     // Declare member variables here. Examples:
     // private int a = 2;
     // private string b = "text";
-    [Export]
-    public PackedScene Mob;
 
-    [Export]
-    public Godot.Collections.Array fishArray;
+
 
     private Random _random = new Random();
-    private Timer fishSpawningTimer;
-    PackedScene fishScene;
+   
 
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        fishSpawningTimer = GetNode<Timer>("FishSpawningTimer");
-        fishSpawningTimer.Start();
-        fishScene = ResourceLoader.Load<PackedScene>("res://actors/FishShadow.tscn");
+        
+  
         
     }
 
-    // We'll use this later because C# doesn't support GDScript's randi().
-    private float RandRange(float min, float max)
-    {
-        return (float)_random.NextDouble() * (max - min) + min;
-    }
+    
+//     public void createFish(FishShadow theFish){
+//         GD.Print("inside top create");
 
-    public void _on_FishSpawningTimer_timeout()
-    {
-        Node2D fishNode = GetNode<Node2D>("YSort/Fish");
-        fishArray = fishNode.GetChildren();
-        GD.Print(fishNode.GetChildCount());
-        GD.Print(fishArray.Count);
-        GD.Print(fishArray[0]);
+//         // Choose a random location on Path2D.
+//         var fishSpawnLocation = GetNode<PathFollow2D>("FishPath/FishSpawnLocation");
+//         fishSpawnLocation.Offset = _random.Next();
+//          GD.Print("inside top create 2");
 
-        if(fishArray.Count < 6)
-        {
-            createFish();
-        }
-        else{
-            
-            GetNode<Node2D>("YSort/Fish").GetChild(0).QueueFree();
-        }
+//         // Set the mob's position to a random location.
+//         theFish.Position = fishSpawnLocation.Position;
 
-    }
-    public void createFish(){
-        // Choose a random location on Path2D.
-            var fishSpawnLocation = GetNode<PathFollow2D>("FishPath/FishSpawnLocation");
-            fishSpawnLocation.Offset = _random.Next();
+//  GD.Print("inside top create 3");
+//         // Add some randomness to the direction.
+//         Vector2 direction = new Vector2(_random.Next(), _random.Next());
 
-            // Create a Mob instance and add it to the scene.
-            FishShadow fishInstance = (FishShadow)fishScene.Instance();
-            GetNode<Node2D>("YSort/Fish").AddChild(fishInstance);
+//  GD.Print("inside top create 4");
+//         GD.Print("direction world" + direction);
+//         theFish.setDirection(direction);
+//          GD.Print("inside top create 5");
 
-            // Set the mob's position to a random location.
-            fishInstance.Position = fishSpawnLocation.Position;
-
-            // Add some randomness to the direction.
-            /*Vector2 direction = new Vector2(_random.Next(), _random.Next());
-            if(direction == new Vector2())
-                direction = new Vector2(0, 1);
-            fishInstance.set_direction(direction);
-            */
-
-            // Choose the velocity.
-            //fishInstance.MoveAndCollide(new Vector2(RandRange(150f, 250f), 0).Rotated(direction));
-    }
+//     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
--- actors/FishShadow.cs
+++ actors/FishShadow.cs
@@ -1,20 +1,37 @@
 using Godot;
 using System;
+using System.Text;
 
 public class FishShadow : KinematicBody2D
 {
-    Vector2 velocity = Vector2.Zero;
+    const float SEPARATION_WEIGHT = 0.5f;
+    const float ALIGNMENT_WEIGHT = 0.5f;
+    const float COHESION_WEIGHT = 0.1f;
+
+    Vector2 _velocity = Vector2.Zero;
+    Vector2 _direction = new Vector2(0, 1);
+    int _separation_distance = 20;
+
+    Godot.Collections.Array fishArray;
 
     [Export]
-    public int MinSpeed = 150; // Minimum speed range.
+    public float _maxSpeed = 1f;
 
     [Export]
-    public int MaxSpeed = 250; // Maximum speed range.
+    public float _speed = 0.5f;
+
+    KinematicCollision2D collision;
+
     static private Random _random = new Random();
     [Export]
     public Boolean isDebug = true;
 
     Label debugLabel;
+
+    private Timer fishSpawningTimer;
+    [Export]
+    public int nbFish;
+
 
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
@@ -25,12 +42,14 @@ public class FishShadow : KinematicBody2D
             animationPlayer.Play("Spin");
         } 
 
+        fishSpawningTimer = GetNode<Timer>("FishSpawningTimer");
+        fishSpawningTimer.WaitTime = _random.Next(2,8);
+        fishSpawningTimer.Start();
+
         float scaleNb = (float)(_random.NextDouble() * (2.5 - 1) + 1);
         Vector2 scale = new Vector2(scaleNb, scaleNb);
         this.Scale = scale;
         isDebug = true;
-        
-
     }
     public void debug(){
         if(!isDebug){
@@ -39,12 +58,90 @@ public class FishShadow : KinematicBody2D
         if(debugLabel == null){
             debugLabel = GetNode<Label>("debugLabel");
         }
-        debugLabel.Text = Position.ToString();
+        debugLabel.Text = this.Name;
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(float delta)
     {
-        debug();
+        if(Visible)
+        {
+            Rotation = new Vector2(0,1).AngleToPoint(_direction);
+            var temp =  _direction * _speed;
+
+            collision = MoveAndCollide(temp);
+            if(collision != null){
+                if(collision.Collider is TileMap){
+                    _direction = _collision_reaction_direction(collision);
+                }
+            }
+            else{
+                //_direction = _flock_direction();
+            }
+
+            debug();
+        }
+    }
+    // Inverts the direction when hitting a collider.
+    // This implementation handles colliding with Tilemaps specifically.
+    public Vector2 _collision_reaction_direction(KinematicCollision2D collision){
+        return (collision.Position - collision.Normal).DirectionTo(this.Position);
+    }
+    /*
+    public Vector2 _flock_direction(){
+        var separation = new Vector2();
+        var heading = _direction;
+        var cohesion = new Vector2();
+
+    }*/
+
+    public void _on_FishSpawningTimer_timeout()
+    {
+        fishSpawningTimer.WaitTime = _random.Next(2,7);
+        // Node fishNode = this.GetParent();
+        // fishArray = fishNode.GetChildren();
+        // nbFish = fishArray.Count;
+        // GD.Print(fishArray.Count);
+        // int fishIdx = _random.Next(nbFish);
+
+        // FishShadow theFish = (FishShadow)GetNode<Node2D>("YSort/Fish").GetChild(fishIdx);
+        GD.Print(this.Name + " timed out");
+        if(this.Visible == false)
+        {
+           
+            GD.Print("the visible is false");
+        }
+
+        if(this.Visible == true)
+        {
+            this.Visible = false;
+            GD.Print("delete fish (set to false)");
+        }
+        else{
+            GD.Print("before visible");
+            this.Visible = true;
+            GD.Print("before create");
+            //createFish(); 
+            GD.Print("create fish (set to true)"); 
+        }
+    }
+        public void createFish(){
+        GD.Print("inside top create");
+
+        // Choose a random location on Path2D.
+        var fishSpawnLocation = GetParent().GetParent().GetParent().GetNode<PathFollow2D>("FishPath/FishSpawnLocation");
+        fishSpawnLocation.Offset = _random.Next();
+
+
+        // Set the mob's position to a random location.
+        this.Position = fishSpawnLocation.Position;
+
+        // Add some randomness to the direction.
+        Vector2 direction = new Vector2(_random.Next(), _random.Next());
+
+
+        _direction = direction;
+
+
     }
 }
